73346a5c82270bc1b602cad5e27647ecc044ef57
mesciuS <giovadev@gmail.com>
2023-05-09 16:29:33 +0200

    feat: header base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/main')
 @section('content')
     <div class="container">
-        prova
+        
     </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,24 @@
 <header>
-    
+    <div class="top-bar">
+
+    </div>
+    <nav>
+        <div>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
+        </div>
+        <div>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div> 
+    </nav>
 </header>
