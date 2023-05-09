3023bd45ec1e15e88307f8328fc25b91110f0247
mesciuS <giovadev@gmail.com>
2023-05-09 17:37:58 +0200

    feat: foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts/main')
 @section('content')
     <div class="container">
-        
+        @foreach ($comics as $comic) 
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">{{$comic['title']}}</p>
+                </div>
+              </div>
+        </div>
+        @endforeach
     </div>
 @endsection
